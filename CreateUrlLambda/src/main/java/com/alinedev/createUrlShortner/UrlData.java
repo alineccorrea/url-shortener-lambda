348a57afb989ececf48d8ceb7bdf64c5414465b9
alineccorrea <alineccorrea97@gmail.com>
2024-11-24 18:46:21 -0300

    Primeiro commit - carregando projeto

---
+++ CreateUrlLambda/src/main/java/com/alinedev/createUrlShortner/UrlData.java
@@ -0,0 +1,17 @@
+package com.alinedev.createUrlShortner;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UrlData {
+
+    private String originalUrl;
+    private long expirationTime;
+
+}
